1f83b0ba6d6676543a0d01ee9fe8eef9a67dbcc4
Administrator <Administrator@pc.localdomain>
2018-09-26 16:25:14 +0200

    fase3: primera parte de el inicio de sesion

---
+++ configuracion.php
@@ -0,0 +1,7 @@
+<!--cada dolar es una variable en php-->
+<?php
+
+$servidor = 'localhost';
+$bbdd = 'evau2018';
+$usuario_mysql = 'root';
+$clave_mysql = '';
--- index.php
+++ index.php
@@ -12,7 +12,7 @@ and open the template in the editor.
     </head>
     <body style="background-color: #666666"></body>
     <body>
-        <div class="container">
+        <div class="container" id = "principal">
             <div class="row">
                 <div class="col-12">
                     <h2 class="text-center" style="color: white;">Ejemplo inicio sesion php</h2>
@@ -24,36 +24,33 @@ and open the template in the editor.
                 
             </div>
                  <div class="col-4">
+                     
                      <br><br>
-                     <form action="index2.php">
-                         <input  id="cajaNombre" class="form-control"  type="text" placeholder="Usuario" required="required">
+                     <input  id="cajaNombre" class="form-control"  type="text" placeholder="Usuario" required="required">
                      <br>
                      <input  id="cajaPassword"  class="form-control" type="password" placeholder="contraseña" required="required">
                      <br>
                      <button id="boton1" class="btn btn-primary btn-block" type="submit">Primary</button>
                      <br><br>
-                      <p>
-                          Year (yyyy-mm-dd):
-                          <input class="form-control" data-validation="date" data-validation-format="yyyy-mm-dd">
-  </p>
-                     </form>
+              
+                   
                  </div>
-                 <div class="col-4"></div>
+                <div class="col-4">
+                </div>
                 </div>
             </div>
-                             
-        <?php
-        // put your code here
-        ?>
     </body>
+    
+    
+    
     <script src="js/jquery-3.3.1.min.js" type="text/javascript"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery-form-validator/2.3.26/jquery.form-validator.min.js"></script>
     
     <script>
         //document ready se ejecuta cuando toda la pagina se a cargado correctamente
-       // $(document).ready(function(){
+      $(document).ready(function(){
        //     $('#cajaNombre').hide();
-        //});
+        });
         
         
     
@@ -63,11 +60,9 @@ and open the template in the editor.
         
     
     
-       // $('#boton1').click(function(){
-       //     console.log("aqui si que llega");
-       //     $('#cajaNombre').show(200);
-       //     
-       // });
+      $('#boton1').click(function(){
+          $('#principal').load("login.php");
+        });
         
         
         
