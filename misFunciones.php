3a8c58a4118cc45c697c01211bf2f60466858bd4
Administrator <Administrator@pc.localdomain>
2018-09-27 18:06:16 +0200

    fase5:hemos conectado las bbdds

---
+++ misFunciones.php
@@ -0,0 +1,8 @@
+<?php
+//conecta la bbdd
+function conectaBBDD(){
+    require('configuracion.php');
+    $mysqli = new mysqli($servidor, $usuario_mysql, $clave_mysql, $bbdd);
+    $mysqli ->query("SET NAMES utf8");
+    return $mysqli;
+}
